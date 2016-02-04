e8d5d28deeef18e2c5282ea1fb5ce423df18c1b6
Christian Bryn <chr.bryn@gmail.com>
2016-02-04 20:06:07 +0100

    It's a class

--- tellprox.py
+++ tellprox.py
@@ -1,57 +1,56 @@
 #!/usr/bin/env python3
 
+
 import requests
 
 DEBUG=False
 
-# class TellProx(host,port = '8080'):
-#   def __init__():
-#     self.host = host
-#     self.port = port
-#   def toggle_device(id):
-#
+class TellProx():
+  def __init__(port='8080', host):
+    self.host = host
+    self.port = port
 
-def toggle_device(id, host, port = '8080'):
-  isnum = id
-  try:
-    isnum += 1
-  except TypeError:
+  def toggle_device(self, id):
+    isnum = id
+    try:
+      isnum += 1
+    except TypeError:
+      return False
+    payload = {'id': id}
+    r = requests.get('http://%s:%s/json/device/toggle?key=&id=%s' % (self.host, self.port, id))
+    if DEBUG:
+      print(r.url)
+    # I think the response needs parsing...but let's assume this works:
+    if r.status_code == requests.codes.ok:
+      return True
     return False
-  payload = {'id': id}
-  r = requests.get('http://%s:%s/json/device/toggle?key=&id=%s' % (host,port,id))
-  if DEBUG:
-    print(r.url)
-  # I think the response needs parsing...but let's assume this works:
-  if r.status_code == requests.codes.ok:
-    return True
-  return False
 
-def enable_device(id, host, port = '8080'):
-  isnum = id
-  try:
-    isnum += 1
-  except TypeError:
+  def enable_device(self, id):
+    isnum = id
+    try:
+      isnum += 1
+    except TypeError:
+      return False
+    payload = {'id': id}
+    r = requests.get('http://%s:%s/json/device/turnon?key=&id=%s' % (self.host, self.port, id))
+    if DEBUG:
+      print(r.url)
+    # I think the response needs parsing...but let's assume this works:
+    if r.status_code == requests.codes.ok:
+      return True
     return False
-  payload = {'id': id}
-  r = requests.get('http://%s:%s/json/device/turnon?key=&id=%s' % (host,port,id))
-  if DEBUG:
-    print(r.url)
-  # I think the response needs parsing...but let's assume this works:
-  if r.status_code == requests.codes.ok:
-    return True
-  return False
 
-def disable_device(id, host, port = '8080'):
-  isnum = id
-  try:
-    isnum += 1
-  except TypeError:
+  def disable_device(self, id):
+    isnum = id
+    try:
+      isnum += 1
+    except TypeError:
+      return False
+    payload = {'id': id}
+    r = requests.get('http://%s:%s/json/device/turnoff?key=&id=%s' % (self.host, self.port, id))
+    if DEBUG:
+      print(r.url)
+    # I think the response needs parsing...but let's assume this works:
+    if r.status_code == requests.codes.ok:
+      return True
     return False
-  payload = {'id': id}
-  r = requests.get('http://%s:%s/json/device/turnoff?key=&id=%s' % (host,port,id))
-  if DEBUG:
-    print(r.url)
-  # I think the response needs parsing...but let's assume this works:
-  if r.status_code == requests.codes.ok:
-    return True
-  return False
