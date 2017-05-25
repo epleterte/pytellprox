29702ba2c578e180b4a4cbb0da5c83855f198164
Christian Bryn <chr.bryn@gmail.com>
2017-05-25 15:04:02 +0200

    add sensor support + fix 'disable_device' bug

--- tellprox.py
+++ tellprox.py
@@ -20,13 +20,14 @@ __author__ = "Christian Bryn"
 __copyright__ = "Copyright 2016, Christian Bryn"
 __credits__ = ["Christian Bryn"]
 __license__ = "GPLv2"
-__version__ = "0.4"
+__version__ = "0.5"
 __maintainer__ = "Christian Bryn"
 __email__ = "chr.bryn@gmail.com"
 __status__ = "Production"
 
 
 import requests
+import json
 import logging
 
 DEBUG=False
@@ -51,15 +52,17 @@ class TellProx():
       return False
     return True
 
-  def _api_get(self, action, id, type='device'):
+  def _api_get(self, action, id=1, type='device'):
     if not self._isnum(id):
       logging.debug("Device id %s is not an integer" % id)
       return False
+    # id does not make sense when listing things, but is ignored...so works with id=1
     r = requests.get('%s://%s:%s/json/%s/%s?key=&id=%s' % (self.protocol, self.host, self.port, type, action, id))
     logging.debug("Request URL: %s" % r.url)
     # I think the response needs parsing...but let's assume this works:
     if r.status_code == requests.codes.ok:
-      return True
+      #return True
+      return r.text
     return False
 
 
@@ -76,9 +79,31 @@ class TellProx():
     return True
 
   def disable_device(self, id):
-    if not self._api_get('turnon', id):
+    if not self._api_get('turnoff', id):
         logging.warning("Could not turnoff device %s" % id)
         return False
     return True
+
+  def list_sensors(self):
+    try:
+      data=self._api_get('list', type='sensors')
+      sensors=json.loads(data)
+    except Exception as e:
+      logging.error("Error listing sensors: %s" % (e))
+      return False
+    return sensors
+
+  def get_sensor(self, id):
+    try:
+      data=self._api_get('info', id, type='sensor')
+      sensor=json.loads(data)
+    except Exception as e:
+      logging.error("Error listing sensor %d: %s" % (id, e))
+      return False
+    return sensor
+
+
+
+
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
